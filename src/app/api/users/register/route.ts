81151f5d3e73ea4aba0091d0e385f7ca26ee1297
gemtunde <gemtunde@gmail.com>
2023-08-28 19:43:11 +0100

    mongodb conections

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -1,19 +1,9 @@
+import { connectDB } from "@/config/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 
-//get
-export async function GET(request: NextRequest) {
-  return NextResponse.json({ message: "get register" });
-}
-
+//db connection
+connectDB();
 //Post
 export async function POST(request: NextRequest) {
   return NextResponse.json({ message: "post register" });
-}
-//Put
-export async function PUT(request: NextRequest) {
-  return NextResponse.json({ message: "pUt register" });
-}
-//delete
-export async function DELETE(request: NextRequest) {
-  return NextResponse.json({ message: "DELETE register" });
 }
